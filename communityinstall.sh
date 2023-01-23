18d67ae88e3ee1fccc7960cc5c3f01097eeaecec
1arrcy1 <69429134+1arrcy1@users.noreply.github.com>
2023-01-23 20:29:00 +0100

    Update communityinstall.sh
    If it gets declined, can i have a comment. I think i made a decent update to it.

--- communityinstall.sh
+++ communityinstall.sh
@@ -1,58 +1,91 @@
 #!/bin/bash
 
+# Root permission check
 if (( $EUID == 1 )); then
-
 echo "Please use root user or sudo ./communityinstall.sh under your non-root account"
 exit
 else
+
+# Settings
+hash -r
+systemctl stop mongod
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+clear
+echo "Welcome to the installation script for h1emu-server"
+echo "Continue at your own risk or use ctl-c to cancel the installation"
+echo ""
+echo "You can request your WORLD_ID on the discord. If you do not know what WORLD_ID is cancel the installation with ctl-c"
+echo -n "please enter your WORLD_ID : "
+read ID
+
+# Installing Dependencies
 echo "Running H1emu.com Community Server installer"
+sleep 2
 apt update && apt upgrade -y
 apt install nodejs npm git net-tools software-properties-common nano node-typescript -y
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 add-apt-repository 'deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse' -y
 apt install mongodb-org -y
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+clear
+
+# Installing pm2-Dependencies
+echo "Installing MongoDB Web GUI"
+sleep 2
 systemctl start mongod
 systemctl enable mongod
-echo "Installing MongoDB Web GUI"
 npm i pm2 -g
-npm i -g mongo-gui
 pm2 kill
+pm2 install pm2-logrotate
+npm i -g mongo-gui
 pm2 start mongo-gui
 npm cache clean -f
 npm install -g n
 n stable
 hash -r
-git clone https://github.com/QuentinGruber/h1z1-server.git
+clear
+
+# Installing h1z1-server
+echo "Downloading and building h1z1-server"
+sleep 2
+git clone https://github.com/H1emu/h1z1-server
 cd h1z1-server
 npm install
-export NODE_ENV="production"
 export DEBUG="ZoneServer"
 export CLIENT_SIXTEEN="true"
 # H1emu LOGINSERVER IS REQUIRED FOR COMMUNITY SERVERS CONTACT @ADMIN & change your WORLD_ID={Provided by h1emu admin}
-export WORLD_ID="2"
-export LOGINSERVER_IP="127.0.0.1"
+export WORLD_ID="$ID"
 export MONGO_URL="mongodb://localhost:27017/"
 pm2 start docker/2016/zoneServer.js --watch
-export DEBUG="*"
-#pm2 start docker/2015/loginServer.js --watch
 pm2 startup
+clear
+
+
+# Creating start script
+echo "export WORLD_ID="$ID"" > start.sh
+echo '# Settings' >> start.sh
 echo 'sysctl -w net.ipv6.conf.all.disable_ipv6=1' >> start.sh
 echo 'sysctl -w net.ipv6.conf.default.disable_ipv6=1' >> start.sh
 echo 'sysctl -w net.ipv6.conf.lo.disable_ipv6=1' >> start.sh
+echo 'echo "stopping old session"' >> start.sh
+echo 'sleep 2' >> start.sh
+echo 'pm2 kill' >> start.sh
+echo 'systemctl stop mongod' >> start.sh
+echo 'clear' >> start.sh
+echo '' >> start.sh
+echo '# Starting server' >> start.sh
+echo 'echo "Starting server"' >> start.sh
+echo 'sleep 2' >> start.sh
+echo 'systemctl start mongod' >> start.sh
 echo 'export DEBUG="ZoneServer"' >> start.sh
 echo 'export CLIENT_SIXTEEN="true"' >> start.sh
 echo '#remove LOGINSERVER if you want to use h1emu community list' >> start.sh
-echo 'export WORLD_ID="2"' >> start.sh
 echo 'export MONGO_URL="mongodb://localhost:27017/"' >> start.sh
-echo 'pm2 kill' >> start.sh
 echo 'pm2 start mongo-gui' >> start.sh
 echo 'pm2 start docker/2016/zoneServer.js --watch' >> start.sh
 echo 'export DEBUG="*"' >> start.sh
 echo 'pm2 startup' >> start.sh
-echo 'sleep 5' >> start.sh
 echo 'echo "**********************************************************************************************" ' >> start.sh
 echo 'echo "**********************************************************************************************" ' >> start.sh
 echo 'echo "**********************************************************************************************" ' >> start.sh
@@ -83,8 +116,6 @@ echo 'echo "********************************************************************
 echo 'echo "**********************************************************************************************" ' >> start.sh
 echo 'echo "**********************************************************************************************" ' >> start.sh
 chmod +x start.sh
-
-sleep 5
 
 echo "**********************************************************************************************" 
 echo "**********************************************************************************************" 
